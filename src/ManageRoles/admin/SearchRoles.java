1559370475baeb6d5e1d69a931c56c3192d1d90a
hitmalith <>
2023-03-14 22:42:18 +0530

    edit search roles

--- src/ManageRoles/admin/SearchRoles.java
+++ src/ManageRoles/admin/SearchRoles.java
@@ -12,7 +12,7 @@ public class SearchRoles {
 
     public static void addNewUserRoleFun() throws InterruptedException {
 
-        boolean roleAded = false;
+        boolean roleSearch = false;
 
         Thread.sleep(1000);
         objEnv.driver.findElement(By.xpath("//*[@id='root']/div[2]/div[1]/div[1]/ul/a[2]/li/span")).click();
@@ -33,9 +33,9 @@ public class SearchRoles {
         objEnv.driver.findElement(By.cssSelector("input")).sendKeys("survey");
 
         Thread.sleep(500);
-        roleAded = objEnv.driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/div/div/div/div[2]/table/tbody/tr/td[2]")).isDisplayed();
+        roleSearch = objEnv.driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/div/div/div/div[2]/table/tbody/tr/td[2]")).isDisplayed();
         
-        if (roleAded) {
+        if (roleSearch) {
             System.out.println("\n\nSearched Roles..!");
         } else {
             System.out.println("\n\nRole Searched Fail..!");
