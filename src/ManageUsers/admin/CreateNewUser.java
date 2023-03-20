842eb8b9e3459bb822f874e0532207d15d7fd5b5
hitmalith <>
2023-03-20 10:29:04 +0530

    created createNewUser.java

--- src/ManageUsers/admin/CreateNewUser.java
+++ src/ManageUsers/admin/CreateNewUser.java
@@ -11,16 +11,18 @@ public class CreateNewUser {
 
     public static void addNewUserRoleFun() throws InterruptedException {
 
-        boolean roleAded = false;
+        boolean createUser = false;
 
         Thread.sleep(1000);
         objEnv.driver.findElement(By.xpath("/html/body/div/div[2]/div[1]/div[1]/ul/a[3]/li/span")).click();
         Thread.sleep(1000);
         objEnv.driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/div/div/div/div[1]/button")).click();
         Thread.sleep(1000);
-        objEnv.driver.findElement(By.xpath("/html/body/div[3]/div/div/div[2]/form/div/input")).click();
+        // objEnv.driver.findElement(By.xpath("/html/body/div[3]/div/div/div[2]/form/div/input")).click();
+        objEnv.driver.findElement(By.name("name")).click();
+        objEnv.driver.findElement(By.name("name")).sendKeys("Malith Chanuka");
         Thread.sleep(1000);
-        objEnv.driver.findElement(By.name("role")).click();
+        // objEnv.driver.findElement(By.name("role")).click();
         {
             WebElement dropdown = objEnv.driver.findElement(By.name("role"));
             dropdown.findElement(By.xpath("//option[. = 'survey creator']")).click();
@@ -33,12 +35,12 @@ public class CreateNewUser {
         objEnv.driver.findElement(By.cssSelector("body > div.fade.add-modal.modal.show > div > div > div.modal-footer > button.success-btn")).click();
 
         Thread.sleep(500);
-        roleAded = objEnv.driver.findElement(By.cssSelector("#root > div.Toastify > div")).isDisplayed();
+        createUser = objEnv.driver.findElement(By.cssSelector("body > div.swal2-container.swal2-center.swal2-backdrop-show > div > div.swal2-actions > button.swal2-confirm.swal2-styled")).isDisplayed();
         
-        if (!roleAded) {
-            System.out.println("\n\nRole Aded..!");
+        if (createUser) {
+            System.out.println("\n\nCreated User..!");
         } else {
-            System.out.println("\n\nRole Aded Fail..!");
+            System.out.println("\n\nNot Created User..!");
         }
     }
 
