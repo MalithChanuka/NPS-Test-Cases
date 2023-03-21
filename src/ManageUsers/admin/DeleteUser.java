de4e324c67872a82a99aa4627971a67308a31991
hitmalith <>
2023-03-21 12:44:20 +0530

    create deleteUser.java

---
+++ src/ManageUsers/admin/DeleteUser.java
@@ -0,0 +1,41 @@
+package ManageUsers.admin;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import app.common.Env;
+
+public class DeleteUser {
+    
+    public static Env objEnv = new Env();
+
+    public static void addNewUserRoleFun() throws InterruptedException {
+
+        boolean createUser = false;
+
+        Thread.sleep(1000);
+        objEnv.driver.findElement(By.cssSelector(".w-100")).click();
+        objEnv.driver.findElement(By.cssSelector("a:nth-child(4) .name")).click();
+
+        Thread.sleep(1000);
+        objEnv.driver.findElement(By.cssSelector("#root > div.users > div.user-container > div > div > div > div.text-center.d-flex.bordertable-wrapper.justify-content-center > table > tbody > tr:nth-child(5) > td:nth-child(5) > svg.icon.close")).click();
+        objEnv.driver.findElement(By.cssSelector(".swal2-confirm")).click();
+        objEnv.driver.findElement(By.cssSelector(".swal2-confirm")).click();
+
+        Thread.sleep(500);
+        createUser = objEnv.driver.findElement(By.cssSelector("body > div.swal2-container.swal2-center.swal2-backdrop-show > div > div.swal2-actions > button.swal2-confirm.swal2-styled")).isDisplayed();
+        
+        if (createUser) {
+            System.out.println("\n\nDeleted User..!");
+        } else {
+            System.out.println("\n\nDid Not Deleted User..!");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        objEnv.setUpDriver();
+        objEnv.signIn();
+        addNewUserRoleFun();
+        objEnv.driver.quit();
+    }
+}
